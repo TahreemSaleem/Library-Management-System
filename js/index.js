0987fd8357291b45dda58718be63f056ec31db78
TahreemSaleem <tahreem.saleem9@gmail.com>
2019-05-28 13:01:16 +0500

    duplicate checks added

--- js/index.js
+++ js/index.js
@@ -1,9 +1,11 @@
+/** entry point of LMS, generates a table from data in local storage 
+ */
 function main() {
     let tbl = document.getElementById('tbl');
-    let tr = document.createElement('tr');
     const items = {...localStorage };
     Object.entries(items).forEach(
         ([key, value]) => {
+            let tr = document.createElement('tr');
             let array = [...new Map(JSON.parse(value)).values()];
             addCell(tr)
             array.forEach((value) => {
@@ -15,7 +17,10 @@ function main() {
         }
     );
 }
-
+/**
+ * extracts parameters from url to know whether to call add logic or update logic
+ * @param {string} url_string   
+ */
 function urlParser(url_string) {
     const url = new URL(url_string);
     const action = url.searchParams.get("action");
@@ -27,10 +32,14 @@ function urlParser(url_string) {
         const array = [...new Map(JSON.parse(map)).values()];
         //TODO: make this better
         [document.getElementById("name").value, document.getElementById("author").value, document.getElementById("publisher").value, document.getElementById("date").value] = array;
-        // var inputs = document.getElementsByTagName('input');
-        // console.log([...inputs])
     }
 }
+
+/**
+ * entry point of Author and Publisher's page, 
+ * extracts parameters from url to decide between the two pages
+ * @param {string} url_string 
+ */
 
 function getList(url_string) {
     const url = new URL(url_string);
@@ -61,8 +70,13 @@ function getList(url_string) {
         }
     );
 }
+/**
+ * removes the book from local storage when author/ publisher removes
+ * @param {object} instance 
+ */
 
 function removeCascade(instance) {
+    console.log(typeof instance)
     let parent = instance.parentNode.parentNode;
     const title = document.getElementById('title').innerHTML;
     const name = parent.childNodes[1].innerText;
@@ -77,14 +91,20 @@ function removeCascade(instance) {
     parent.parentNode.removeChild(parent);
 }
 
-
-
+/**
+ * calls the update page
+ * @param {object} instance 
+ */
 function update(instance) {
     let parent = instance.parentNode.parentNode;
     const key = parent.childNodes[1].innerText;
-    window.location.assign("editbook.html?action=update&key=" + key);
+    window.location.assign(`editbook.html?action=update&key=${key}`);
 }
 
+/**
+ * removes a book
+ * @param {object} instance 
+ */
 function remove(instance) {
     let parent = instance.parentNode.parentNode;
     const name = parent.childNodes[1].innerText;
@@ -92,12 +112,23 @@ function remove(instance) {
     parent.parentNode.removeChild(parent);
 }
 
-function addCell(tr, val = []) {
+/**
+ * inserts a Cell in a table
+ * @param {object} tr table row
+ * @param {string} val value to insert inside cell of table
+ */
+function addCell(tr, val = '') {
     let td = document.createElement('td');
     td.innerHTML = val;
     tr.appendChild(td)
 }
 
+/**
+ * inserts a button in table
+ * @param {object} tr table row
+ * @param {string} value value inside button
+ * @param {string} fnct on click function name
+ */
 function addBtn(tr, value, fnct) {
     let td = document.createElement('td');
     let btn = document.createElement('button');
@@ -108,12 +139,31 @@ function addBtn(tr, value, fnct) {
     tr.appendChild(td);
 }
 
+/**
+ * adds a book data inside local storage
+ */
 function addBook() {
     let book = new Map();
     book.set('name', document.getElementById("name").value);
     book.set('author', document.getElementById("author").value);
     book.set('publisher', document.getElementById("publisher").value);
-    book.set('date', document.getElementById("date").value)
-    window.localStorage.setItem(book.get('name'), JSON.stringify([...book.entries()]))
-    window.location = "index.html";
+    book.set('date', document.getElementById("date").value);
+    if (validate([...book.values()])) {
+        window.localStorage.setItem(book.get('name'), JSON.stringify([...book.entries()]));
+        window.location = "index.html";
+    }
+}
+/**
+ * checks duplicate values
+ * @param {array} book 
+ */
+function validate(book) {
+    const items = {...localStorage };
+    Object.entries(items).forEach(
+        ([key, value]) => {
+            if (JSON.stringify(book).toLowerCase() == JSON.stringify([...new Map(JSON.parse(value)).values()]).toLowerCase()) {
+                alert('duplicate enteries not allowed');
+                return false;
+            }
+        });
 }
